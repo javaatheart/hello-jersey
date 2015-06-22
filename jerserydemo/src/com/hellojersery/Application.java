dbadc837e9421e4ce5a57556002a62017635ae2c
javaatheart <javaatheart@gmail.com>
2015-06-21 23:03:20 -0500

    cleaned it up

--- jerserydemo/src/com/hellojersery/Application.java
+++ jerserydemo/src/com/hellojersery/Application.java
@@ -6,6 +6,7 @@ import com.sun.jersey.api.core.PackagesResourceConfig;
 public class Application  extends PackagesResourceConfig {
 
 	public Application() {
+
 		super("com.hellojersery");
 		System.out.println("This is called during the start up");
 	}
